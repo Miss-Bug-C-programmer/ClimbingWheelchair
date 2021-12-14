4c524a1e239e22551225e4d2ddaa0501c6df61b6
angcx1997 <ang.chin.xian@gmail.com>
2021-12-14 16:38:52 +0800

    Edit sabertooth target number

--- Core/Src/DifferentialDrivetoSabertooth.c
+++ Core/Src/DifferentialDrivetoSabertooth.c
@@ -16,6 +16,6 @@ void differentialDrivetoSabertoothOutputAdapter(differentialDrive_Handler* dd_ha
 
 	int motor_output_1 = dd_handler->m_leftMotor * SABERTOOTH_MAX_ALLOWABLE_VALUE;
 	int motor_output_2 = dd_handler->m_rightMotor * SABERTOOTH_MAX_ALLOWABLE_VALUE;
-	MotorThrottle(st_handler, TARGET_1, motor_output_1);
-	MotorThrottle(st_handler, TARGET_2, motor_output_2);
+	MotorThrottle(st_handler, TARGET_2, motor_output_1);
+	MotorThrottle(st_handler, TARGET_1, motor_output_2);
 }
